87767ead2ae84c76123b1f781112aee11c06faee
PuttTim <yannaputt@gmail.com>
2023-10-08 09:18:35 +0800

    ✨ Added basic ui for cooldown page

--- hol-up-extension/src/pages/CooldownPage.tsx
+++ hol-up-extension/src/pages/CooldownPage.tsx
@@ -1,7 +1,48 @@
-import React from "react"
+import { useState, useEffect } from "react"
+import holdup from "../assets/hold-up-meme.png"
+import { Button } from "@/components/ui/button"
 
 const CooldownPage = () => {
-    return <div>CooldownPage</div>
+    const hostName = "tiktok"
+    const redirectUrl = "https://www.tiktok.com/explore"
+    const cooldown = 3
+    const todayCount = 1
+
+    const [countdown, setCountdown] = useState(cooldown)
+
+    const intervalId = setInterval(() => {
+        if (countdown > 0) {
+            setCountdown(countdown - 1)
+            console.log(countdown)
+            clearInterval(intervalId)
+            return
+        }
+    }, 1000)
+
+    useEffect(() => {
+        return () => {
+            clearInterval(intervalId)
+        }
+    }, [])
+
+    return (
+        <div className="align-center m-auto mt-2 flex w-full flex-col justify-center gap-6">
+            <h1 className="text-6xl font-bold">Hold up!</h1>
+            <img src={holdup} className="hold-up w-3/12"></img>
+
+            {countdown <= 0 ? (
+                <div className="flex w-1/4 flex-col justify-center gap-4">
+                    <div className="text-3xl">
+                        {todayCount} tries to view {hostName} today
+                    </div>
+                    <Button variant="outline">I want to procrastinate. LET ME IN</Button>
+                    <Button>Okay I'll go be productive</Button>
+                </div>
+            ) : (
+                <></>
+            )}
+        </div>
+    )
 }
 
 export default CooldownPage
