12f04af1c6dd118d5d86f59f3176480d78c680d8
Victor Chua <victorchua865@gmail.com>
2023-10-08 11:01:30 +0800

    taking data and filter correctly, changed to delete button

--- hol-up-extension/src/pages/PopUpPage.tsx
+++ hol-up-extension/src/pages/PopUpPage.tsx
@@ -8,22 +8,18 @@ import {
     TableHeader,
     TableRow,
 } from "@/components/ui/table"
-import { useState } from "react"
-import { GrEdit } from "react-icons/gr"
+import { db } from "@/firebase"
+import { BlockedAppApiResponse } from "@/types/BlockedApp"
+import { collection, getDocs, query, where } from "firebase/firestore"
+import { useEffect, useState } from "react"
+import { AiFillDelete } from "react-icons/ai"
 
 export function PopUpPage() {
-    const HolUp = [
-        {
-            name: "Facebook",
-            website: "Facebook",
-            timeLeft: 30,
-        },
-        {
-            name: "Instagram",
-            website: "Instagram",
-            timeLeft: 40,
-        },
-    ]
+    useEffect(() => {
+        getBlockedApps()
+    }, [])
+    const [HolUp, setHolUp] = useState<BlockedAppApiResponse[]>([])
+
     const [filterText, setFilterText] = useState<string>("")
 
     // Function to handle text input change
@@ -36,23 +32,40 @@ export function PopUpPage() {
         item.name.toLowerCase().includes(filterText.toLowerCase()),
     )
 
+    const getBlockedApps = async () => {
+        // FIXME - get actual userId from local storage
+        const q = query(collection(db, "blockedApps"), where("userId", "==", "007"))
+        const querySnapshot = await getDocs(q)
+
+        const blockedApps: BlockedAppApiResponse[] = []
+
+        querySnapshot.forEach((doc) => {
+            const data = doc.data() as BlockedAppApiResponse
+            console.log(">>>", data)
+            blockedApps.push({ ...data, docId: doc.id })
+        })
+
+        setHolUp(blockedApps)
+        console.log(blockedApps)
+    }
+
     return (
         <div className="w-full p-4">
             <Input
-                placeholder="Email"
+                placeholder="Website"
                 type="text"
                 value={filterText}
                 onChange={handleFilterTextChange}
             />
-            <div className="rounded-md border mt-4">
+            <div className="mt-4 rounded-md border">
                 <Table>
                     <TableCaption>A list of your Blocked websites.</TableCaption>
                     <TableHeader>
                         <TableRow>
                             <TableHead className="text-center">Logo</TableHead>
                             <TableHead className="text-center">website</TableHead>
-                            <TableHead className="text-center">Time Left</TableHead>
-                            <TableHead className="text-center">Edit</TableHead>
+                            <TableHead className="text-center">Cooldown</TableHead>
+                            <TableHead className="text-center">Delete</TableHead>
                         </TableRow>
                     </TableHeader>
                     <TableBody>
@@ -60,16 +73,16 @@ export function PopUpPage() {
                             <TableRow key={name.name}>
                                 <TableCell className="font-medium">
                                     <img
-                                        className="w-8 h-8"
+                                        className="h-8 w-8"
                                         src={`https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://www.${name.name}.com/&size=256`}></img>
                                 </TableCell>
-                                <TableCell>{name.website}</TableCell>
-                                <TableCell>{name.timeLeft} min</TableCell>
+                                <TableCell>{name.name}</TableCell>
+                                <TableCell>{name.cooldown} seconds</TableCell>
                                 <TableCell
-                                    className="text-right"
+                                    className="text-center"
                                     // onClick={openScreen}
                                 >
-                                    <GrEdit />
+                                    <AiFillDelete className="m-auto" size={25} />
                                 </TableCell>
                             </TableRow>
                         ))}
